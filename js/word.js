c592ab2626c3333b22f0257fde634f4a78486a5a
mercygsk <mercygs.kodali@gmail.com>
2023-10-30 14:37:37 -0400

    list of words

---
+++ js/word.js
@@ -0,0 +1,198 @@
+let wordList = [
+    {
+        word: "Nomophobia",
+        hint: "Anxiety about lack of access to mobile"
+    },
+    {
+        word: "Tuba",
+        hint: "a musical instrument"
+    },
+    {
+        word: "Broigus",
+        hint: "angry or irritated"
+    },
+    {
+        word: "earth",
+        hint: "planet of our solar system"
+    },
+    {
+        word: "platinum",
+        hint: "a white precious metal"
+    },
+    {
+        word: "amazon",
+        hint: "online shopping site"
+    },
+    {
+        word: "javascript",
+        hint: "programming language"
+    },
+    {
+        word: "coding",
+        hint: "related to programming"
+    },
+    {
+        word: "ironMan",
+        hint: "science fiction movie"
+    },
+    {
+        word: "code",
+        hint: "related to programming"
+    },
+    {
+        word: "avatar",
+        hint: "epic science fiction film"
+    },
+    {
+        word: "jpg",
+        hint: "a file format for image"
+    },
+    {
+        word: "mental",
+        hint: "related to the mind"
+    },
+    {
+        word: "cryptocurrency",
+        hint: "virtual or digital currency"
+    },
+    {
+        word: "island",
+        hint: "land surrounded by water"
+    },
+    {
+        word: "soccer",
+        hint: "a famous outdoor game"
+    },
+    {
+        word: "chess",
+        hint: "related to a indoor game"
+    },
+    {
+        word: "twitter",
+        hint: "a social media app"
+    },
+    {
+        word: "github",
+        hint: "code hosting platform"
+    },
+    {
+        word: "png",
+        hint: "a image file format"
+    },
+    {
+        word: "lol",
+        hint: "Abbreviated form for 'Laugh'"
+    },
+    {
+        word: "computer",
+        hint: "an electronic device"
+    },
+    {
+        word: "cpu",
+        hint: "computer component"
+    },
+    {
+        word: "java",
+        hint: "programming language"
+    },
+    {
+        word: "google",
+        hint: "famous search engine"
+    },
+    {
+        word: "Topophilia",
+        hint: "an emotional connection to a place"
+    },
+    {
+        word: "excel",
+        hint: "microsoft product for windows"
+    },
+    {
+        word: "mysql",
+        hint: "a relational database system"
+    },
+    {
+        word: "USA",
+        hint: "developed country name"
+    },
+    {
+        word: "flute",
+        hint: "a musical instrument"
+    },
+    {
+        word: "crypto",
+        hint: "related to cryptocurrency"
+    },
+    {
+        word: "tesla",
+        hint: "unit of magnetic flux density"
+    },
+    {
+        word: "mars",
+        hint: "planet of our solar system"
+    },
+    {
+        word: "approval",
+        hint: "Having a positive opinion of something"
+    },
+    {
+        word: "email",
+        hint: "related to exchanging message"
+    },
+    {
+        word: "html",
+        hint: "markup language for the web"
+    },
+    {
+        word: "air",
+        hint: "related to a gas"
+    },
+    {
+        word: "idea",
+        hint: "a thought or suggestion"
+    },
+    {
+        word: "server",
+        hint: "related to computer or system"
+    },
+    {
+        word: "svg",
+        hint: "a vector image format"
+    },
+    {
+        word: "jpeg",
+        hint: "a image file format"
+    },
+    {
+        word: "search",
+        hint: "act to find something"
+    },
+    {
+        word: "Jeopardize",
+        hint: "the endangerment to a person or situation"
+    },
+    {
+        word: "Hostile",
+        hint: " an unfriendly person or situation."
+    },
+    {
+        word: "Chip",
+        hint: "a small and thin piece of a larger item"
+    },
+    {
+        word: "Jug",
+        hint: "a utensil or container used to hold liquids"
+    },
+    {
+        word: "photo",
+        hint: "representation of person or scene"
+    },
+    {
+        word: "nile",
+        hint: "largest river in the world"
+    },
+    {
+        word: "rain",
+        hint: "related to a water"
+    },
+]
